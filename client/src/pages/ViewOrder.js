701c0bbd3be6bfcc0373c8f3adb54872b215731c
Malcolm McQueen <110631738+mmcqueen92@users.noreply.github.com>
2023-09-16 22:34:20 -0700

    fix dependency array in vieworder useeffect

--- client/src/pages/ViewOrder.js
+++ client/src/pages/ViewOrder.js
@@ -1,25 +1,25 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import { useEffect, useState } from "react";
 
 export default function ViewOrder(props) {
-    const {id} = useParams();
-    const [orderInfo, setOrderInfo] = useState();
+  const { id } = useParams();
+  const [orderInfo, setOrderInfo] = useState();
 
+  useEffect(() => {
     const fetchOrder = async () => {
-        const response = await fetch(`http://localhost:5050/api/find-order/${id}`);
-        const data = await response.json();
-        return data;
-      };
+      const response = await fetch(
+        `http://localhost:5050/api/find-order/${id}`
+      );
+      const data = await response.json();
+      return data;
+    };
+    fetchOrder().then((res) => setOrderInfo(res[0]));
+  }, [id]);
 
-    useEffect(() => {
-        fetchOrder().then((res) => setOrderInfo(res[0]));
-    })
-
-    return(
-        <div>
-            <h3>Order {id}</h3>
-            <p>Order Status: {orderInfo ? orderInfo.status : "...loading"}</p>
-
-        </div>
-    )
+  return (
+    <div>
+      <h3>Order {id}</h3>
+      <p>Order Status: {orderInfo ? orderInfo.status : "...loading"}</p>
+    </div>
+  );
 }
