2b2211e134d033f011d045c81a7ccd70bdf0233e
Malcolm McQueen <110631738+mmcqueen92@users.noreply.github.com>
2023-09-30 02:10:28 -0700

    contact us html and css

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,10 +1,19 @@
 export default function Contact() {
-    return (
-        <div>
-            This is the Contact page<br/>
-            phone number goes here<br/>
-            email goes here<br/>
-            social media goes here<br/>
-        </div>
-    )
+  return (
+    <div className="contact-us-page">
+      <h1 className="contact-us-title">Contact Us</h1>
+      <div className="contact-info">
+        <p className="contact-address">Address: 1337 Burgers St.</p>
+        <p className="contact-phone">
+          Phone: <a href="tel:555-555-5555">555-555-5555</a>
+        </p>
+        <p className="contact-email">
+          Email:{" "}
+          <a href="mailto:burgersbunsandbeyond@email.com">
+            burgersbunsandbeyond@email.com
+          </a>
+        </p>
+      </div>
+    </div>
+  );
 }
